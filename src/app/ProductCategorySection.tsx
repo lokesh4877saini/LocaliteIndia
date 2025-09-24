a38a0d941a86e8d0f8a4599de9ac687f7d941e07
RahulSharam87420 <rahuaharma088@gmail.com>
2025-09-24 10:51:11 +0530

    image update  ProductGallary file update

--- src/app/ProductCategorySection.tsx
+++ src/app/ProductCategorySection.tsx
@@ -1,45 +1,66 @@
-import Image from "next/image";
+import Link from "next/link";
 function ProductCategorySection() {
   return (
-    <> 
-    <section>
+    <>
+      <section>
+        <div className="tab_header_color flex items-center mb-4">
+          <div className="flex items-center ">
+            <h2 className="text-2xl font-bold">
+              New Arrivals
+            </h2>
+          </div>
 
-     <div className="tab_header_color flex items-center justify-between mb-4">
-   <div className="flex items-center justify-between">
-      <h2 className="text-2xl font-bold">New <br /> Arrivals</h2>
-    </div>
-        
-   <ul className="flex space-x-3  no-scrollbar-hide">
-     
-    <li><a href="ProductCategorySection" className="px-4 py-2 rounded-md bg-black text-white font-medium  whitespace-nowrap">New</a></li> 
-     
-    <li><a href="ProductCategorySection" className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100 whitespace-nowrap">Best Sellers</a></li> 
-     
-    <li><a href="ProductCategorySection" className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">All Co-ords </a></li> 
-     
-    <li><a href="ProductCategorySection"className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">All Dresses</a></li> 
-     
-    <li><a href="ProductCategorySection" className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">All Kurtas</a></li> 
-     
-    <li><a href="ProductCategorySection" className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">Ankle Leggings</a></li> 
-     
-    <li><a href="ProductCategorySection" className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100 whitespace-nowrap">Editions</a></li> 
-     
-    <li><a href="ProductCategorySection"className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">Organza</a></li> 
-     
-    <li><a href="ProductCategorySection" className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">Suit Sets</a></li> 
-     
-    <li><a href="#ProductCategorySection" className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">Festive</a></li> 
-     
-    <li><a href="#ProductCategorySection" className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap">Straight Pant</a></li> 
-     
-   </ul>
-    
-    </div>
-  
+          <ul className="flex space-x-3 m-5 min-width-110">
+            <button className="px-2 bg-black text-white text-sm font-medium rounded-none">
+              <Link
+                href="#"
+                className="rounded-md text-gray-400  text-white font-medium  hover:bg-gray-100 whitespace-nowrap"
+              />
+              New
+            </button>
 
-    </section>
+            <button className="bg-black text-white text-sm font-medium px-2 py-1 rounded-none">
+              <Link
+                href="#"
+                className="rounded-md w-full  text-gray-700 font-medium hover:bg-gray-100 whitespace-nowrap"
+              />
+              Best Sellers
+            </button>
+
+            <button className="px-2 bg-black text-white text-sm font-medium rounded-none">
+              <Link
+                href="#"
+                className="rounded-md w-full text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"
+              />
+              All Co-ords{" "}
+            </button>
+
+            <button className=" px-2 bg-black text-white text-sm font-medium hover:bg-gray-100 whitespace-nowrap">
+              <Link
+                href="#"
+                className=" rounded-md w-full text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"
+              />
+              All Dresses
+            </button>
+
+            {/* <button><Link href="#" className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"/>All Kurtas</button> 
+     
+    <button><Link href="#" className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"/>Ankle Leggings</button> 
+     
+    <button><Link href="#" className=" px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100 whitespace-nowrap"/>Editions</button> 
+     
+    <button><Link href="#"className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"/>Organza</button> 
+     
+    <button><Link href="#" className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"/>Suit Sets</button> 
+     
+    <button><Link href="#" className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"/>Festive</button> 
+     
+    <button><Link href="#" className="px-4 py-2 rounded-md w-full border border-gray-300 text-gray-700 font-medium hover:bg-gray-100  whitespace-nowrap"/>Straight Pant</button> 
+      */}
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
-export default ProductCategorySection;
+export default ProductCategorySection
--- src/app/ProductGallary.tsx
+++ src/app/ProductGallary.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 function ProductGallary() {
   return (
     <>
@@ -6,127 +7,157 @@ function ProductGallary() {
         <div className="px-60">
           <div className="grid grid-cols-5 gap-2 place">
             <span className="">
-              <a href="/collections/new-arrivals" className="w-full">
+              <Link href="/collections/new-arrivals" className="w-full">
                 <div className="collection-gallery-img flex flex-col items-center text-center padding-right:100px margin-bottom:60px">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-15_1.png?v=1755764905"
+                  <Image
+                    src={'/product/productImage2.jpg'}
                     className=" w-30 h-40"
+                    alt="Image"
+                    width={150}
+                    height={150}
                   />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   Sale
                 </div>
-              </a>
+              </Link>
             </span>
-            <div className="">
-              <a href="/collections/bestsellers">
+              <div className="">
+              <Link href="/collections/bestsellers">
                 <div className="collection-gallery-img flex flex-col items-center text-center padding-right:10 margin-bottom:100">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-11_540ffd15-7ffb-43bf-a653-c92e739ada63.png?v=1755760978"
-                    className=" w-30 h-40"
-                  />
+                  <Image
+                    src={'/product/productImage.jpeg'}
+                    className="w-30 h-40"
+                    alt="Image"
+                    width={150}
+                    height={150}
+                    />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   New Arrivals
                 </div>
-              </a>
+              </Link>
             </div>
             <div className="">
-              <a href="/collections/bestsellers">
+              <Link href="/collections/bestsellers">
                 <div className="collection-gallery-img flex flex-col items-center text-center ">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-10.png?v=1755760978"
+                  <Image
+                    src={'/product/productImage.jpeg'}
                     className=" w-30 h-40 "
-                  />
+                    alt="Image"
+                    width={150}
+                    height={150}
+                    />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   Best Sellers
                 </div>
-              </a>
+              </Link>
             </div>
+            
             <div className="">
-              <a href="/collections/dresses">
+              <Link href="/collections/dresses">
                 <div className="collection-gallery-img flex flex-col items-center text-center">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-09.png?v=1755760978"
+                  <Image
+                    src={'/product/productImage.jpeg'}
                     className=" w-30 h-40 "
+                     alt="Image"
+                    width={150}
+                    height={150}
                   />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   All Dresses
                 </div>
-              </a>
-            </div>
+              </Link>
+            </div> 
 
             <div className="">
-              <a href="/collections/casual-wear">
+              <Link href="/collections/casual-wear">
                 <div className="collection-gallery-img flex flex-col items-center text-center ">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-08_025e9825-5b7b-43e4-ace8-0566c33272e3.png?v=1755760979"
-                    className=" w-30 h-40 "
+                  <Image
+                    src={'/product/productImage.jpeg'}
+                      className=" w-30 h-40 "
+                     alt="Image"
+                    width={150}
+                    height={150}
                   />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   All Kurtas
                 </div>
-              </a>
+              </Link>
             </div>
+            
             <div className="">
-              <a href="/collections/co-ords">
+              <Link href="/collections/co-ords">
                 <div className="collection-gallery-img flex flex-col items-center text-center">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-07_43655763-e92f-4af3-a895-1c30253cfd28.png?v=1755760978"
-                    className=" w-30 h-40 "
+                  <Image
+                    src={'/product/productImage.jpeg'}
+                      className=" w-30 h-40 "
+                     alt="Image"
+                    width={150}
+                    height={150}
                   />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   All Co-Ords
                 </div>
-              </a>
+              </Link>
             </div>
 
             <div className="">
-              <a href="/collections/maternity">
+              <Link href="/collections/maternity">
                 <div className="collection-gallery-img flex flex-col items-center text-center">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-14_c5e768cb-f8a1-4c89-a6e5-b689b2d220cd.png?v=1755760979"
+                  <Image
+                    src={'/product/productImage.jpeg'}
                     className=" w-30 h-40"
+                     alt="Image"
+                    width={150}
+                    height={150}
                   />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   Maternity
                 </div>
-              </a>
+              </Link>
             </div>
 
             <div className="">
-              <a href="/collections/editions">
+              <Link href="/collections/editions">
                 <div className="collection-gallery-img flex flex-col items-center text-center">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-20_1.png?v=1752148190"
-                    className="w-30 h-40"
+                  <Image
+                  src={'/product/productImage.jpeg'}
+                  className=" w-30 h-40"
+                   alt="Image"
+                    width={150}
+                    height={150}
                   />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   Resort Wear
                 </div>
-              </a>
+              </Link>
             </div>
 
             <div className="">
-              <a href="/collections/organza">
+              <Link href="/collections/organza">
                 <div className="collection-gallery-img flex flex-col items-center text-center">
-                  <img
-                    src="//www.hayclothing.in/cdn/shop/files/ci-12.png?v=1755760978"
-                    className="w-30 h-40"
+                  <Image
+                  src={'/product/productImage.jpeg'}
+                  className=" w-30 h-40"
+                    alt="Image"
+                    width={150}
+                    height={150}
                   />
                 </div>
                 <div className="overlay-text__heading-vsb rounded-2xl p-4 text-center">
                   Organza
                 </div>
-              </a>
+              </Link>
             </div>
           </div>
+
         </div>
 
       </section>
